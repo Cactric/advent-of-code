48fa92ce1fb7035f5c266ab92bd6dfa8648421f9
Cactric <59334627+Cactric@users.noreply.github.com>
2022-12-09 23:58:05 +0000

    Day 9, wrong part 2

--- 2022/9/solve.hs
+++ 2022/9/solve.hs
@@ -2,36 +2,38 @@ import Data.Char
 import Data.List.Split
 import Data.List
 
+type Knot = (Int, Int)
+
 readLines :: FilePath -> IO [String]
 readLines = fmap lines . readFile
 
-catchUp :: (Int, Int) -> (Int, Int) -> (Int, Int)
+catchUp :: Knot -> Knot -> Knot
 catchUp (hx, hy) (tx, ty) = if mags < 2^2 then (tx, ty) else ((tx + dx), (ty + dy)) where
     dx = signum (hx - tx)
     dy = signum (hy - ty)
     mags = (hx - tx)^2 + (hy - ty)^2
 
-doLeft :: Int -> (Int, Int) -> [(Int, Int)] -> [(Int, Int)]
+doLeft :: Int -> Knot -> [Knot] -> [Knot]
 doLeft 0 h ts = (catchUp h (head ts)):ts
 doLeft n (hx, hy) ts = doLeft (n-1) nh $ (catchUp nh (head ts)):ts where
     nh = (hx-1, hy)
 
-doDown :: Int -> (Int, Int) -> [(Int, Int)] -> [(Int, Int)]
+doDown :: Int -> Knot -> [Knot] -> [Knot]
 doDown 0 h ts = (catchUp h (head ts)):ts
 doDown n (hx, hy) ts = doDown (n-1) nh $ (catchUp nh (head ts)):ts where
     nh = (hx, hy - 1)
 
-doUp :: Int -> (Int, Int) -> [(Int, Int)] -> [(Int, Int)]
+doUp :: Int -> Knot -> [Knot] -> [Knot]
 doUp 0 h ts = (catchUp h (head ts)):ts
 doUp n (hx, hy) ts = doUp (n-1) nh $ (catchUp nh (head ts)):ts where
     nh = (hx, hy + 1)
 
-doRight :: Int -> (Int, Int) -> [(Int, Int)] -> [(Int, Int)]
+doRight :: Int -> Knot -> [Knot] -> [Knot]
 doRight 0 h ts = (catchUp h (head ts)):ts
 doRight n (hx, hy) ts = doRight (n-1) nh ((catchUp nh (head ts)):ts) where
     nh = (hx + 1, hy)
 
-doAction :: String -> (Int, Int) -> [(Int, Int)] -> [(Int, Int)]
+doAction :: String -> Knot -> [Knot] -> [Knot]
 doAction a h ts = 
     if d == "L" then doLeft n h ts else
     if d == "R" then doRight n h ts else
@@ -40,7 +42,7 @@ doAction a h ts =
         d = head(splitOn " " a)
         n = (read (head(tail (splitOn " " a))))
 
-moveHead :: String -> (Int, Int) -> (Int, Int)
+moveHead :: String -> Knot -> Knot
 moveHead a (hx, hy) = 
     if d == "L" then (hx-n, hy) else
     if d == "R" then (hx+n, hy) else
@@ -50,13 +52,50 @@ moveHead a (hx, hy) =
         n = read((splitOn " " a) !! 1)
 
 -- Actions → Head → Old tails → New tails
-ropeAction :: [String] -> (Int, Int) -> [(Int, Int)] -> [(Int, Int)]
+ropeAction :: [String] -> Knot -> [Knot] -> [Knot]
 ropeAction [] _ ts = ts
 ropeAction (a:as) h ts = ropeAction as (moveHead a h) (doAction a h ts)
 
 -- Returns the number of positions the tail visits at least once
 ropeSimulate :: [String] -> Int
 ropeSimulate x = length (nub (ropeAction x (0,0) [(0,0)]))
+
+polyChase :: Knot -> [[Knot]] -> [[Knot]]
+polyChase h [] = []
+polyChase h (ts:tss) = ((catchUp h (head ts)):ts):(polyChase (head ts) tss)
+
+polyLeft :: Int -> Knot -> [[Knot]] -> [[Knot]]
+polyLeft 0 h tss = (polyChase h tss)
+polyLeft n (hx, hy) tss = polyLeft (n-1) nh $ polyChase nh tss where
+    nh = (hx-1, hy)
+polyRight :: Int -> Knot -> [[Knot]] -> [[Knot]]
+polyRight 0 h tss = (polyChase h tss)
+polyRight n (hx, hy) tss = polyRight (n-1) nh $ polyChase nh tss where
+    nh = (hx+1, hy)
+polyDown :: Int -> Knot -> [[Knot]] -> [[Knot]]
+polyDown 0 h tss = (polyChase h tss)
+polyDown n (hx, hy) tss = polyDown (n-1) nh $ polyChase nh tss where
+    nh = (hx, hy-1)
+polyUp :: Int -> Knot -> [[Knot]] -> [[Knot]]
+polyUp 0 h tss = (polyChase h tss)
+polyUp n (hx, hy) tss = polyUp (n-1) nh $ polyChase nh tss where
+    nh = (hx, hy+1)
+
+polyAction :: String -> Knot -> [[Knot]] -> [[Knot]]
+polyAction a h tss =
+    if d == "L" then polyLeft n h tss else
+    if d == "R" then polyRight n h tss else
+    if d == "U" then polyUp n h tss else
+    {-if d == "D" then-} polyDown n h tss where
+        d = head(splitOn " " a)
+        n = (read ((splitOn " " a) !! 1))
+
+polyknot :: [String] -> Knot -> [[Knot]] -> [[Knot]]
+polyknot [] h tss = tss
+polyknot (a:as) h tss = polyknot as (moveHead a h) (polyAction a h tss)
+
+tentaKnot :: [String] -> Int
+tentaKnot x = length (nub (last (polyknot x (0,0) (replicate 9 [(0,0)]))))
 
 main :: IO()
 main = do
@@ -65,5 +104,5 @@ main = do
     putStr "Part one: "
     putStrLn $ show (ropeSimulate fileContents)
     
-    putStr "Part two: "
-    putStrLn "?"
+    putStr "Part two (not correct): "
+    putStrLn $ show $ tentaKnot fileContents
