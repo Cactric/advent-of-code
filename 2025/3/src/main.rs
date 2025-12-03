fe262bd408b24f98c889ce15687b7661d8c2e167
Cactric <59334627+Cactric@users.noreply.github.com>
2025-12-03 14:51:48 +0000

    Finish part 1 for simple_input

--- 2025/3/src/main.rs
+++ 2025/3/src/main.rs
@@ -18,23 +18,55 @@ fn main() {
         if bank == "" {
             continue;
         }
+
+        // Get the highest joltage, split the list at that the first appearance of it, then
+        // choose the largest one on the right
+        // If none, choose the largest digit on the left
+        let joltages: Vec<(usize, char)> = bank.chars().enumerate().collect();
+        let mut highest: (usize, char) = (0, '\0');
+        for j in &joltages {
+            if j.1 > highest.1 {
+                highest = *j;
+            }
+        }
+        let mut second_highest: (usize, char) = (0, '\0');
+        let (before, after) = joltages.split_at(highest.0);
+        // If the 'after' part contains more than just the highest we just found
+        if after.len() > 1 {
+            // Choose the largest one on the right
+            // Excluding the highest already used (after[0])
+            for j in &after[1..] {
+                if j.1 > second_highest.1 {
+                    second_highest = *j;
+                }
+            }
+            let bank_joltage = highest.1.to_digit(10).expect("Highest digit wasn't a digit") * 10 +
+                               second_highest.1.to_digit(10).expect("Second highest wasn't a digit");
+            println!("Bank joltage: {}", bank_joltage);
+            jolt_sum += bank_joltage;
+        } else {
+            // Choose the largest one on the left
+            for j in before {
+                if j.1 > second_highest.1 {
+                    second_highest = *j;
+                }
+            }
+            let bank_joltage = second_highest.1.to_digit(10).expect("Second highest wasn't a digit") * 10 +
+                               highest.1.to_digit(10).expect("Highest digit wasn't a digit");
+            println!("Bank joltage: {}", bank_joltage);
+            jolt_sum += bank_joltage;
+        }
         
+        /*
         // Sort the joltages of the batteries, using enumerate to have a reference
         // for the initial order
-        let mut joltages: Vec<(usize, char)> = bank.chars().enumerate().collect();
         joltages.sort_unstable_by_key(|(_i,c)| *c);
         
-        // Get the highest two joltages and sort them by position
+        
         let mut highest_two = joltages.split_at(joltages.len() - 2).1.to_vec();
         highest_two.sort();
         println!("{:?}", highest_two);
-        let mut bank_joltage = 0;
-        for (_i, j) in highest_two {
-            bank_joltage *= 10;
-            bank_joltage += j.to_digit(10).unwrap();
-        }
-        println!("Bank joltage: {}", bank_joltage);
-        jolt_sum += bank_joltage;
+        */
     }
     
     eprintln!("Joltage sum: {}", jolt_sum);
