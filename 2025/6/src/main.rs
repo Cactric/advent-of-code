70c5b137a487c809ce937dcb18d361e89c4b5a80
Cactric <59334627+Cactric@users.noreply.github.com>
2025-12-06 12:19:12 +0000

    Day 6, part 2

--- 2025/6/src/main.rs
+++ 2025/6/src/main.rs
@@ -2,6 +2,15 @@ use std::fs::read_to_string;
 use std::env;
 use std::process::exit;
 
+fn is_all_spaces(s: &str) -> bool {
+    for c in s.chars() {
+        if c != ' ' {
+            return false;
+        }
+    }
+    return true;
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let filename;
@@ -10,7 +19,7 @@ fn main() {
         Some(n) => filename = n
     }
     let number_list = read_to_string(filename).expect("Couldn't read input file");
-    let numbers: Vec<&str> = number_list.trim_end().split("\n").collect();
+    let numbers: Vec<&str> = number_list.trim_end_matches('\n').split("\n").collect();
     
     // Split the input by whitespace (which conveniently handles multiple spaces for me)
     let mut split_nums: Vec<Vec<&str>> = numbers.iter().map(|s| s.split_ascii_whitespace().collect()).collect();
@@ -41,4 +50,34 @@ fn main() {
     }
     
     println!("Total (part 1): {}", total);
+    
+    // Part 2
+    // Make a list of columns (transpose)
+    let mut transposed_numbers = Vec::new();
+    for col in 0..numbers[0].len() {
+        let mut t_row: Vec<u8> = Vec::new();
+        for row in 0..(numbers.len()-1) {
+            t_row.push(numbers[row].as_bytes()[col]);
+        }
+        transposed_numbers.push(String::from_utf8(t_row).unwrap());
+    }
+    
+    let t_matrix: Vec<&[String]> = transposed_numbers.split(|s| is_all_spaces(s)).collect();
+    
+    let mut part2_total = 0;
+    // Go backwards through the transposed matrix of numbers
+    for x in (0..operations.len()).rev() {
+        let op = operations[x];
+        let num_list = t_matrix[x].iter().map(|s| s.trim().parse::<u64>().expect("Not a number"));
+        // Reversing the num_list is unnecessary here, since + and * are both symmetric
+        part2_total += match op {
+            "*" => num_list.rev().fold(1, |acc, x| acc * x),
+            "+" => num_list.rev().fold(0, |acc, x| acc + x),
+            unknown => {
+                eprintln!("Unknown operation {}", unknown);
+                exit(2);
+            }
+        }
+    }
+    println!("Total (part 2): {}", part2_total);
 }
